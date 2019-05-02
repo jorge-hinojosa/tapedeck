557976a33ebeafc0a663d2093aab740f464b25b2
Jorge Hinojosa <47377561+jorge-hinojosa@users.noreply.github.com>
2019-05-02 10:19:27 -0500

    Delete aws_config.js

--- src/aws_config.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  bucketName: "honey-bucket",
-  dirName: "projects",
-  region: "us-west-2",
-  accessKeyId: "AKIAIRGLY2N7DRDPZT3Q",
-  secretAccessKey: "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"
-};
